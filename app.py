c98449073bb343fe1ac214259995eb5f415cb09a
wipip <166628878+wipip@users.noreply.github.com>
2026-02-26 07:24:29 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-From PIL import Image
+from PIL import Image
 
 st.title("israel bomb site")
 
@@ -11,3 +11,12 @@ st.image(image, caption="they look like this")
 
 texto= st.text.input('escriba algo','este es el texto')
 st.write("el ttexto escrito es", texto)
+
+col1, col2 = st.colums(2)
+
+with col1:
+  st.subheader("hola, q mas")
+  st.write("hablelo")
+  resp=st.checkbox("sizas")
+  if resp:
+    st.write("así es!")
